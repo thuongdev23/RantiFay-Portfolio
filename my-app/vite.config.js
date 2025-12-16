1a72e83c4fab235a85ec409c7e053196f558311c
thuongdev23 <thuongvannguyen233@gmail.com>
2025-12-16 22:07:16 +0300

    fix vite config

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          motion: ["framer-motion"]
+        }
+      }
+    }
+  }
+});
